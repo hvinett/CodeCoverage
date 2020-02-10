89f1950ac962fe838db412ffa9a0c36b462f5051
Vineeth Hanumanth <39301309+hvinett@users.noreply.github.com>
2020-02-10 15:15:44 +0530

    moified cc

---
+++ CodeCoverage/Module1/Module1Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    public class Module1Class1
+    {
+        public int Module1Class1Method1()
+        {
+            return 1;
+        }
+
+    }
+}
---
+++ CodeCoverage/Module1/Module1Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    public class Module1Class2
+    {
+        public int Module1Class2Method1(int result)
+        {
+            if(result > 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return 1;
+        }
+    }
+}
